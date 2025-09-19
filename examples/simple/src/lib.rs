b07e62aa916f516df415ecedfea147430c2b41fc
Marc Guiselin <marc@guiselin.com>
2025-09-19 15:58:03 +0200

    Another api change, but keep things simple this time

--- examples/simple/src/lib.rs
+++ examples/simple/src/lib.rs
@@ -1,54 +1,59 @@
 use bevy_transform::components::Transform;
-use wit_bindgen::rt::async_support;
 
 mod bindings {
+    // This could be replaced with wit_bindgen::generate!() in the future,
+    // since it can resolve the path and world from Cargo.toml
+    // But it currently has issues resolving wasvy:ecs...
+    // almost like it ignores [package.metadata.component.target.dependencies]
     wit_bindgen::generate!({
-        path: "../../wit/ecs/ecs.wit",
-        world: "guest",
+        path: ["../../wit/ecs", "./wit"],
+        world: "component:simple/example",
+        with: {
+            "wasvy:ecs/app": generate,
+            "wasvy:ecs/types": generate,
+            "wasvy:ecs/system-params": generate,
+        }
     });
 }
-use bindings::{wasvy::ecs::api::*, *};
+use bindings::{
+    wasvy::ecs::{
+        app::{App, System},
+        system_params::{Commands, QueryResults},
+        types::{Query, Schedule},
+    },
+    *,
+};
 
 struct GuestComponent;
 
 impl Guest for GuestComponent {
     fn setup() {
         // Define a new system that queries for entities with a Transform and a Marker component
-        let my_system = SystemBuilder::new("my_system");
+        let my_system = System::new("my_system");
         my_system.add_query(&[
             Query::Mut("bevy_transform::components::Transform".to_string()),
             Query::With("host_example::Marker".to_string()),
         ]);
-        let (my_system, mut stream) = my_system.build();
 
         // Register the system to run in the Update schedule
-        let m = Mod::new("simple");
-        m.add_systems(Schedule::Update, vec![my_system]);
+        let app = App::new();
+        app.add_systems(Schedule::Update, vec![my_system]);
+    }
 
-        // Start listening to the event stream
-        async_support::spawn(async move {
-            loop {
-                let input = match stream.next().await {
-                    Some(e) => e,
-                    None => break,
-                };
+    fn my_system(_commands: Commands, query: QueryResults) -> () {
+        loop {
+            let results = match query.iter() {
+                Some(e) => e,
+                None => break,
+            };
 
-                let query = input.next().as_query();
-                loop {
-                    let results = match query.iter() {
-                        Some(e) => e,
-                        None => break,
-                    };
+            let mut transform: Transform = serde_json::from_str(&results[0].get()).unwrap();
 
-                    let mut transform: Transform = serde_json::from_str(&results[0].get()).unwrap();
+            // Simply rotate the entity a bit on each frame
+            transform.rotate_x(2.0);
 
-                    // Simply rotate the entity a bit on each frame
-                    transform.rotate_x(2.0);
-
-                    results[0].set(&serde_json::to_string(&transform).unwrap());
-                }
-            }
-        });
+            results[0].set(&serde_json::to_string(&transform).unwrap());
+        }
     }
 }
 
