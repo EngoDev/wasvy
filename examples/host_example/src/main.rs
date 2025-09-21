f7fde1d73e849a91ffaff5795d3d910905fb3099
Marc Guiselin <marc@guiselin.com>
2025-09-21 16:35:39 +0200

    Remove these bevy plugins that arn't compatible with the release candidate

--- examples/host_example/src/main.rs
+++ examples/host_example/src/main.rs
@@ -1,6 +1,5 @@
 use bevy::prelude::*;
 use bevy::{DefaultPlugins, app::App};
-use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 
 // Get started by importing the prelude
 use wasvy::prelude::*;
@@ -11,9 +10,6 @@ fn main() {
         .add_plugins((
             // Next, add the [`ModloaderPlugin`] ;)
             ModloaderPlugin,
-            // Plus some helpers for the example
-            EguiPlugin,
-            WorldInspectorPlugin::new(),
         ))
         .add_systems(Startup, startup)
         .run();
